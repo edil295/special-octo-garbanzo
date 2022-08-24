944f75b80f615fda699752199fb51b12617b2e3a
edil295 <72927448+edil295@users.noreply.github.com>
2022-08-24 08:19:26 +0600

    Update admin.py

--- complex/admin.py
+++ complex/admin.py
@@ -11,22 +11,11 @@ def count_of_apartment(obj):
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
     date_hierarchy = 'date_of_sale'
-    # actions_on_bottom = True
-    # actions_on_top = False
     empty_value_display = 'Без хозяина'
-    # exclude = ("date_of_sale",)
-    # fields = ('name', )
-    # fields = ('date_of_sale', 'name', ('area',  'apart_block'))
     list_display = ('name', 'date_of_sale', 'status', 'area', count_of_apartment)
-    # list_display_links = ('group', )
     list_editable = ('date_of_sale', )
     list_filter = ('status', 'client_block')
-    # list_per_page = 2
-    # ordering = ('date_of_sale', )
-    # save_as = True
-    # save_on_top = True
     search_fields = ('name',)
-    # sortable_by = ('date_of_sale', )
 
 
 @admin.display(description='Итоговая стоимость квартир')
